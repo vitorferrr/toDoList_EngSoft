aaccf4cc84c0a1214f0c5c88bda1e30a5e5903bc
Vitor Ferreira <vitorferr1412@gmail.com>
2025-11-07 17:43:05 +0000

    correção server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '.')));
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use('/api/v1/users', userRoutes); 
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
